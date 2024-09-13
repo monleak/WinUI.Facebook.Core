e41b7882d3988307753692e7b1cd701ef8e6c126
monleak <hieu16112001@gmail.com>
2024-09-14 02:19:33 +0700

    create model facebook account

---
+++ Models/Facebook_Account.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinUI.Facebook.Core.Models;
+
+public enum Facebook_AccountStatus
+{
+    Live,
+    Banned,
+    Verify,
+    Default
+}
+
+public enum Facebook_CookieStatus
+{
+    Live,
+    Die,
+    Default
+}
+
+public class Facebook_Account
+{
+    public string username
+    {
+        get; set;
+    }
+    public string password
+    {
+        get; set;
+    }
+
+    public string twoFA
+    {
+        get; set;
+    }
+
+    public string cookies
+    {
+        get; set;
+    }
+
+    public Facebook_AccountStatus status_account
+    {
+        get; set;
+    }
+    public Facebook_CookieStatus status_cookie
+    {
+        get; set;
+    }
+
+    public Facebook_Account(string[] rawDatas)
+    {
+        status_account = Facebook_AccountStatus.Default;
+        status_cookie = Facebook_CookieStatus.Default;
+        username = rawDatas[0];
+        password = rawDatas[1];
+        if(rawDatas.Length > 2)
+        {
+            for (int i = 2; i < rawDatas.Length; i++)
+            {
+                if (rawDatas[i].Contains("c_user"))
+                {
+                    cookies = rawDatas[i];
+                    break;
+                }
+            }
+            if (!rawDatas[2].Contains("c_user"))
+            {
+                twoFA = rawDatas[2];
+            }
+        }
+    }
+}
