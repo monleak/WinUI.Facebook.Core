7b2f8b224df542268b7eaa87d13cb69b960ba9cf
monleak <hieu16112001@gmail.com>
2024-09-14 02:30:39 +0700

    add service read raw data account facebook

---
+++ Contracts/Services/IFacebook_ReadDataService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WinUI.Facebook.Core.Models;
+
+namespace WinUI.Facebook.Core.Contracts.Services;
+public interface IFacebook_ReadDataService
+{
+    Task<IEnumerable<Facebook_Account>> ReadRawDataAsync(string pathFile);
+}
---
+++ Services/Facebook_ReadDataService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WinUI.Facebook.Core.Contracts.Services;
+using WinUI.Facebook.Core.Models;
+
+namespace WinUI.Facebook.Core.Services;
+public class Facebook_ReadDataService : IFacebook_ReadDataService
+{
+    public async Task<IEnumerable<Facebook_Account>> ReadRawDataAsync(string pathFile)
+    {
+        IEnumerable<Facebook_Account> accounts = new ObservableCollection<Facebook_Account>();
+        if (File.Exists(pathFile))
+        {
+            var datas = File.ReadLines(pathFile);
+            foreach (var data in datas)
+            {
+                var arrDataSplit = data.Split('|');
+                if (arrDataSplit.Length < 2)
+                {
+                    continue;
+                }
+                accounts.Append(new Facebook_Account(arrDataSplit));
+            }
+            return accounts;
+        }
+        await Task.CompletedTask;
+        return default;
+    }
+}
